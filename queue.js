83e15f993c724f9fa26dead19fac8791cb8440c0
Seth Vargas <shvargas01@gmail.com>
2023-10-26 22:26:38 -0500

    Finished queues

--- queue.js
+++ queue.js
@@ -20,26 +20,53 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
+    const newNode = new Node(val);
 
+    if (this.isEmpty()) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+
+    this.size++;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
+    if (this.isEmpty()) {
+      throw new Error("Queue is empty.");
+    }
 
+    const removedValue = this.first.val;
+
+    this.first = this.first.next;
+    this.size--;
+
+    if (this.size === 0) {
+      this.last = null;
+    }
+
+    return removedValue;
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
+    if (this.isEmpty()) {
+      throw new Error("Queue is empty.");
+    }
 
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0;
   }
 }
 
